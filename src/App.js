b2ce58a4a1c068106057da65d774ec9acd47b30b
Carlos Alencar <68407281+alencardotpy@users.noreply.github.com>
2023-01-13 10:06:43 -0300

    Taildwind init

---
+++ src/App.js
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <div className="h-screen w-screen bg-slate-300 flex items-center">
+      <div className="m-auto hover:animate-fade rounded-lg max-sm:w-8/12 max-md:w-10/12 max-lg:w-6/12 max-xl:w-6/12 max-2xl:w-4/12 bg-slate-500 h-3/5">
+        .
+      </div>
+      <div></div>
+    </div>
+  );
+}
+
+export default App;
+/*
+      My balance $921.48 Spending - Last 7 days mon tue wed thu fri sat sun
+      Total this month $478.33 +2.4% from last month
+      */
